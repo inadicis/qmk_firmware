7fa76d61452a073d8bf2b4785883853a8e020049
Nils Cichy <nils.cichy@check24.de>
2025-05-24 21:58:59 +0200

    move home row mod down

--- keyboards/klornew/keymaps/default/keymap.c
+++ keyboards/klornew/keymaps/default/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    ┌─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤ │╰╯╰╯╰╯╰╯╰╯╰╯╰╯╰╯│ ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┐
    │Ctrl/Esc │    A    │    S    │    D    │    F    │    G    ├─╯                ╰─┤    H    │    J    │    K    │    L    │    ;    │    '    │
    ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤╭────────╮╭────────╮├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
-   │    `    │    Z    │    X    │    C    │    V    │    B    ││ PlayPau││ NextTra││    N    │    M    │  Alt/,  │ Shift/. │OSM Shift│Caps Word│
+   │    `    │    Z    │ Shift/X │ Opt/C   │ Cmd/V   │    B    ││ PlayPau││ NextTra││    N    │    M    │  Alt/,  │ Shift/. │OSM Shift│Caps Word│
    └─────────┴─────────┴─────────┼─────────┼─────────┼─────────┼╰────────╯╰────────╯┼─────────┼─────────┼─────────┼─────────┴─────────┴─────────┘
                                  │ Meh     │ L2/Space│ L1/Tab  │  Enter  ││   TG(3) │  Bspc   │ OSL(1)  │  Hyper  │
                                  └─────────┴─────────┴─────────┴─────────┘└─────────┴─────────┴─────────┴─────────┘ */
@@ -41,8 +41,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    [_BASE_QWERTZ] = LAYOUT_polydactyl(
               KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                          KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
-MT(MOD_LCTL, KC_ESC),KC_A,MT(MOD_LSFT, KC_S),MT(MOD_LALT, KC_D),MT(MOD_LGUI, KC_F),    KC_G,                          KC_H,      KC_J,     KC_K,    KC_L,     KC_SEMICOLON,  KC_QUOTE,
-KC_GRAVE,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_MPLY,   KC_MNXT,  KC_N,     MT(MOD_LGUI, KC_M),     MT(MOD_LALT, KC_COMMA), MT(MOD_LSFT, KC_DOT),   OSM(MOD_LSFT), CW_TOGG,
+MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D,  KC_F,    KC_G,                          KC_H,      KC_J,     KC_K,    KC_L,     KC_SEMICOLON,  KC_QUOTE,
+KC_GRAVE,  KC_Z, MT(MOD_LSFT, KC_X), MT(MOD_LALT, KC_C), MT(MOD_LGUI,KC_V),     KC_B,     KC_MPLY,   KC_MNXT,  KC_N,     MT(MOD_LGUI, KC_M),     MT(MOD_LALT, KC_COMMA), MT(MOD_LSFT, KC_DOT),   OSM(MOD_LSFT), CW_TOGG,
   KC_MEH, LT(2, KC_SPACE), LT(1, KC_TAB), KC_ENTER,                         TG(3), KC_BSPC, OSL(1), KC_HYPR
  ),
 
