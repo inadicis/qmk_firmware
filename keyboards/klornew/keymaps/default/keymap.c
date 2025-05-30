86e3cb594061c64b3f987b68a72c4dd84dc31cff
Nils Cichy <nils.cichy@check24.de>
2025-05-30 18:23:32 +0200

    wip

--- keyboards/klornew/keymaps/default/keymap.c
+++ keyboards/klornew/keymaps/default/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤╭────────╮╭────────╮├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
    │    `    │    Z    │ Shift/X │     C   │     V   │    B    ││ PlayPau││ NextTra││    N    │    M    │  Alt/,  │ Shift/. │OSM Shift│Caps Word│
    └─────────┴─────────┴─────────┼─────────┼─────────┼─────────┼╰────────╯╰────────╯┼─────────┼─────────┼─────────┼─────────┴─────────┴─────────┘
-                                 │ osm Meh │ L2/Space│ L1/Tab  │Cmd/Enter││   Alt   │  Bspc   │ OSL(1)  │osm Hyper│
+                                 │ osm Meh │ L2/Space│ L1/Tab  │alt/Enter││   cmd   │  Bspc   │ OSL(1)  │osm Hyper│
                                  └─────────┴─────────┴─────────┴─────────┘└─────────┴─────────┴─────────┴─────────┘ */
     //
 
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                          KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
 MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D,  KC_F,    KC_G,                          KC_H,      KC_J,     KC_K,    KC_L,     KC_SEMICOLON,  KC_QUOTE,
 KC_GRAVE,  KC_Z, MT(MOD_LSFT, KC_X),  KC_C, KC_V,     KC_B,     KC_MPLY,   KC_MNXT,  KC_N,     MT(MOD_LGUI, KC_M),     MT(MOD_LALT, KC_COMMA), MT(MOD_LSFT, KC_DOT),   OSM(MOD_LSFT), CW_TOGG,
-  KC_MEH, LT(2, KC_SPACE), LT(1, KC_TAB), MT(MOD_LGUI, KC_ENTER),                         OSM(MOD_LALT), KC_BSPC, OSL(1), KC_HYPR
+        KC_MEH, LT(2, KC_SPACE), LT(1, KC_TAB), MT(MOD_LALT, KC_ENTER),                         MOD_LGUI, KC_BSPC, OSL(1), KC_HYPR
  ),
 
     // TODO:  hg combo : escape
